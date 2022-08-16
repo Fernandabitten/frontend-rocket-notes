1eb6a7190468b911779f3973243d826bfb5dc449
Fernandabitten <fernandab.bittencourt@gmail.com>
2022-08-16 19:14:36 -0300

    up

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import {Container}  from './style';
+import { Button } from '../../components/Button';
+
+export function Details() { 
+  return (
+    <Container>
+        <h1>Hello Word</h1>
+        <Button />
+    </Container>
+  )
+}
