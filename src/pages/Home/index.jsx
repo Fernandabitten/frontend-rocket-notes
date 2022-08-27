df6329e126bee27c6ec6e95ab100f8002788020f
Fernandabitten <fernandab.bittencourt@gmail.com>
2022-08-27 18:04:06 -0300

    applying filters

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
-import { RiSearch2Fill } from "react-icons/ri";
+import { FiPlus } from "react-icons/fi";
 import {Container, Brand, Menu, Search, Content, NewNote}  from './style';
 
 import { Header } from "../../components/Header";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
   return(
@@ -11,7 +12,10 @@ export function Home() {
       </Brand>
       <Header />
       <Menu>
-
+        <li><ButtonText title="Todos" isActive/></li>
+        <li><ButtonText title="FrontEnd" /></li>
+        <li><ButtonText title="Node" /></li>
+        <li> <ButtonText title="React" /></li>   
       </Menu>
 
       <Search>
@@ -23,7 +27,8 @@ export function Home() {
       </Content>
 
       <NewNote>
-
+        <FiPlus />
+        Criar nota
       </NewNote>
     </Container>
   )
