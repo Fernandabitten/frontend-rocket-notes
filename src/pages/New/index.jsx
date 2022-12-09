75f843443d569783ec14bb9b0960b5abf1940c4b
Fernandabitten <fernandab.bittencourt@gmail.com>
2022-12-09 07:44:47 -0300

    feat: new page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,47 @@
+import {Container, Form}  from './style';
+
+import {Header} from '../../components/Header';
+import {Input} from '../../components/Input';
+import {Textarea} from '../../components/Textarea';
+import {NoteItem} from '../../components/NoteItem';
+import {Section} from '../../components/Section';
+import {Button} from '../../components/Button';
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>     
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input 
+            placeholder='Titulo'
+          />
+
+          <Textarea 
+            placeholder='Observações'
+          />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+          </Section>
+
+          <Section title="Marcadores">
+            <div className='tags'>
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="Nova tag"/>
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
